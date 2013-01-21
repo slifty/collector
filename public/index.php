ce7b857400c35517b04e90d24a0c746f7bc5509e
slifty <slifty@gmail.com>
2013-01-21 07:07:23 -0500

    added attribution

--- public/index.php
+++ public/index.php
@@ -5,8 +5,26 @@
 		<script src="js/lib/jquery-1.9.0.min.js"></script>
 		<script src="js/collector.js"></script>
 		<script>
+			function getParameterByName(name) {
+				name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+				var regexS = "[\\?&]" + name + "=([^&#]*)";
+				var regex = new RegExp(regexS);
+				var results = regex.exec(window.location.search);
+				if(results == null)
+					return "";
+				else
+					return decodeURIComponent(results[1].replace(/\+/g, " "));
+			}
 			$(function() {
-				$("#results").collector("test");
+				$("#collectButton").click(function() {
+					$("#results").collector($("#tagText").val());
+					$("#questions").hide();
+				});
+
+				if(getParameterByName("cd")) {
+					$("#results").collector(getParameterByName("cd"));
+					$("#questions").hide();
+				}
 			});
 		</script>
 	</head>
@@ -14,19 +32,23 @@
 	<body>
 		<div id="content">
 			<h1>The Collector</h1>
-			<p>This service will aggregate a series of tweets within a hash tag to genearte a single document made up of those tweets.  Results may vary depending on the hash tag.  If you would like to orchestrate the generation of a specific collective document check out The Replicator.</p>
+			<p>When you think of a document you probably think of a book, a file, or a page that you could print out.  It stands alone, it is licensed as a unit, and it is easy to take down, censor, or destroy.  This makes a document pretty weak.</p>
+			<p>What if the document wasn't defined by one person, one file, or one website?  What if it was defined by a group of people spread across the world, controlled by a collective?</p>
+			<p>This page will generate a collective document before your eyes.  More specifically, your browser is scanning Twitter for specially crafted tweets.  The tweets contain instructions for the creation of the new collective document.</p>
+			<p>You can render collective documents anywhere on the open web using <a href="https://github.com/slifty/collector/blob/master/public/js/collector.js">this code</a>.  You can create a collective document of your own using <a href="https://github.com/slifty/replicator/blob/master/public/js/replicator.js">this code</a>.  If you don't feel like setting it up yourself you can just visit <a href="http://replicator.istheinternetabigtruck.com">The Replicator</a>.</p>
+			<hr />
 			<form>
-				<ul>
+				<ul id="questions">
 					<li>
-						<label for="tagText">#</label>
-						<input id="tagText" type="text" />
-					</li>
-					<li>
-						<input id="collectButton" type="button" value="Collect" />
+						<label for="tagText">Collective Document ID</label>
+						<input id="tagText" type="text" /> (begins with "CD_")
 					</li>
 				</ul>
+				<div id="results">
+					<div id="collectButton">Render Collective Document</div>
+				</div>
 			</form>
-			<div id="results"></div>
 		</div>
+		<div id="attribution">This was built by <a href="http://slifty.com">Dan Schultz</a> (<a href="http://www.twitter.com/slifty">@slifty</a>) and Paul Schultz (<a href="https://twitter.com/paulwschultz">@paulwschultz</a>).  The code is <a href="https://github.com/slifty/collector">on Github.</a></div>
 	</body>
 </html>
--- public/js/collector.js
+++ public/js/collector.js
@@ -1,5 +1,37 @@
 (function($) {
-	$.collector = function(el, term, options){
+	var CHAR_MAP = [33,34,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190];
+	var ascii_to_int = function(string) { // right now assumes it will either be 1 or two chars long)
+		if(string.length == 1)
+			return CHAR_MAP.indexOf(string.charAt(0));
+		if(string.length == 2)
+			return CHAR_MAP.indexOf(string.charCodeAt(0)) * CHAR_MAP.length + CHAR_MAP.indexOf(string.charCodeAt(1));
+    }
+
+    var int_to_ascii = function(x, character_count) {
+        if(x > CHAR_MAP.length*CHAR_MAP.length)
+            return ("[ERR: TOO LARGE]");
+        if(x < CHAR_MAP.length && character_count != 2)
+            return String.fromCharCode(CHAR_MAP[x]);
+        return String.fromCharCode(CHAR_MAP[Math.floor(x/CHAR_MAP.length)]) + String.fromCharCode(CHAR_MAP[x % CHAR_MAP.length]);
+    }
+
+
+    var parse_tweet = function(text) {
+    	var message = "";
+    	var checksum = "";
+    	var checksum_index = text.lastIndexOf(' ') + 1;
+    	var message_end_index = text.lastIndexOf('#') - 1;
+    	if(checksum_index != -1 && (checksum_index + 7 <= text.length) && text[checksum_index] == int_to_ascii(1))
+    		checksum = text.substr(checksum_index, 7);
+    	if(message_end_index >= 1)
+    		message = text.substr(0,message_end_index);
+		return {
+			'message': message,
+			'checksum': checksum
+		}
+	}
+
+	$.collector = function(el, identifier, options){
 		var base = this;
 
 		// Access to jQuery and DOM versions of element
@@ -11,32 +43,67 @@
 
 		// Set up instance variables
 		base.tweets = [];
+		base.texts = [];
 		base.refresh_url = '';
+		base.total_texts = 0;
+
 
 		base.init = function(){
-			if( typeof( term ) === "undefined" || term === null ) term = "";
+			if( typeof( identifier ) === "undefined" || identifier === null ) identifier = "";
 
-			base.term = term;
+			base.identifier = identifier;
 
 			base.options = $.extend({},$.collector.defaultOptions, options);
+
+			base.$el.empty();
+
+			var $instructions = $("<div />")
+                .addClass("instructions")
+                .appendTo(base.$el);
+
+            var $headline = $("<h2 />")
+                .text("Document " + base.identifier)
+                .appendTo($instructions);
+
+            var $details1 = $("<p />")
+                .html("Below is a collective document.  It is being generated based on messages sent to Twitter.  You can share this document ")
+                .appendTo($instructions);
+
+            var $helpLink = $("<a />")
+                .addClass("link")
+                .attr("target", "_blank")
+                .attr("href", window.location.href.split('?')[0] + "?cd=" + base.identifier)
+                .text("using this link.")
+                .appendTo($details1);
+
+            var $details2 = $("<p />")
+                .html("Green content is verified, red content is unverified, black content is currently missing.")
+                .appendTo($instructions);
+
+
+			var $tweets = $("<div />")
+				.addClass("tweets")
+				.appendTo(base.$el);
+			base.$tweets = $tweets;
 
 			base.load("http://search.twitter.com/search.json", 1);
 
 		};
 
 		base.load = function(url, max_depth) {
-			console.log(url);
 			$.ajax({
 				dataType: "jsonp",
 				url: url,
 				data: {
-					q: base.term,
+					q: base.identifier,
 					rpp: 100
 				}
 			})
 			.done(function(data) {
-
-				console.log(data);
+				for(var x in data.results) {
+					tweet = data.results[x];
+					base.tweets[tweet.id_str] = tweet;
+				}
 
 				// Refresh the document
 				base.refresh();
@@ -52,8 +119,86 @@
 			})
 		}
 
-		base.refresh() = function() {
+		base.refresh = function() {
+			var verify_tweets = function(tweetBuckets) {
+				var approved_data = [];
+				for(var x in tweetBuckets) {
+					var bucket = tweetBuckets[x];
+					for(var y in bucket) {
+						var next_data = bucket[y];
+						if(next_data.checksum.length==7) { // Sanity check
+							if(!((x==0?tweetBuckets.length:x)-1 in approved_data)) {
+								next_data.isVerified = 0;
+								approved_data[x] = next_data;
+							} else if (verify_checksums(approved_data[(x==0?tweetBuckets.length:x)-1], next_data)) {
+								next_data.isVerified = 1;
+								approved_data[x] = next_data;
+							}
+						}
+					}
+				}
+				return approved_data;
+			}
 
+			var verify_checksums = function(d1,d2) {
+				if(d1 == null || d2 == null || d1.checksum < 7 || d2.checksum < 7)
+					return false;
+				var m1 = d1["message"];
+				var c_sum1 = d1["checksum"];
+				var g_sum1 = generate_mod_checksum(m1);
+				var m2 = d2["message"];
+				var c_sum2 = d2["checksum"];
+				var g_sum2 = generate_mod_checksum(m2);
+				return c_sum1[0] == c_sum2[0] && g_sum1 == c_sum2[1] && g_sum2 == c_sum1[2] && c_sum1.substr(3,2) == c_sum2.substr(3,2);
+			}
+
+			var generate_mod_checksum = function(text) {
+				total = 1;
+				for(var x in text) {
+					var ch = text[x];
+					total = (total * ch.charCodeAt(0)) % 61
+				}
+				return int_to_ascii(total);
+			}
+
+			var tweetBuckets = [];
+			for(var x in base.tweets) {
+				var tweet = base.tweets[x];
+				var parsed_tweet = parse_tweet(tweet.text);
+				var index = ascii_to_int(parsed_tweet.checksum.substr(5,2));
+				if(!(index in tweetBuckets))
+					tweetBuckets[index] = []
+
+				tweetBuckets[index].push(parsed_tweet);
+			}
+
+			var verified_tweets = verify_tweets(tweetBuckets);
+			var slot_count = 0;
+
+			for(var x in verified_tweets) {
+				var tweet = verified_tweets[x];
+				slot_count = ascii_to_int(tweet.checksum.substr(3,2));
+			}
+
+			for(var x = 0; x < slot_count ; ++x) {
+				var $container = $("#cd_" + x);
+				if($container.length == 0) {
+					$container = $("<span />")
+						.attr("id", "cd_" + x)
+						.addClass("tweet")
+						.addClass("empty")
+						.html("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ")
+						.appendTo(base.$tweets);
+				}
+				if(x in verified_tweets) {
+					var tweet = verified_tweets[x];
+					if($container.text != tweet.message)
+						$container.text(tweet.message + " ");
+
+					$container.removeClass()
+						.addClass(tweet.isVerified?"verified":"unverified");
+				}
+			}
 		}
 
 		// Run initializer
@@ -64,9 +209,9 @@
 		caching: false
 	};
 
-	$.fn.collector = function(term, options){
+	$.fn.collector = function(identifier, options){
 		return this.each(function(){
-			(new $.collector(this, term, options));
+			(new $.collector(this, identifier, options));
 		});
 	};
 })(jQuery)
